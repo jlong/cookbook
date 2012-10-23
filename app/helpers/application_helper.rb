951a6c5be0dc33b887f0dcb4eae3eb4e4df6af1e
John W. Long <me@johnwlong.com>
2012-10-22 20:56:03 -0400

    require a title for a recipe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def errors_for(model, attribute)
+    content_tag :span, model.errors[attribute].first, class: 'help-block' unless model.errors[attribute].blank?
+  end
+
+  def error_class(model, attribute)
+    'error' unless @recipe.errors[:title].blank?
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :title, :instructions
+  
   belongs_to :created_by, class_name: User
   belongs_to :updated_by, class_name: User
+
+  validates_presence_of :title
 end
